4bd7adf484e0cce4e92fa233414cd68f947a8cb6
AntonioVladimir <antortechin@gmail.com>
2020-10-12 01:11:27 +0200

    Create arrayClaveValor.js

---
+++ entornoCliente/scripts/arrayClaveValor.js
@@ -0,0 +1,12 @@
+const objetoConValores = {
+    a:1,
+    b:2,
+    c:3,
+    d:4
+};
+
+var solucion  = Object.entries(objetoConValores);
+
+console.log(solucion);
+
+//console.log(Object.entries(objetoConValores).map(([key,valor]) =>[+key,valor]));
