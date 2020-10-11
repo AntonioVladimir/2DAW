3b23b84b284e50d4d7b26c54def1ca6f976eba2f
AntonioVladimir <antortechin@gmail.com>
2020-10-12 01:17:31 +0200

    Create arrayConDosElementos.js

---
+++ entornoCliente/scripts/arrayConDosElementos.js
@@ -0,0 +1,11 @@
+const objetoConValores = {
+    a:1,
+    b:2,
+    c:3,
+    d:4
+};
+
+var claves = Object.keys(objetoConValores);
+var valores = Object.values(objetoConValores);
+
+console.log([claves, valores]);
